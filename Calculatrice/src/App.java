511df579aa6fb1ae932559c2f6e7cc86ce4aba66
rdaoud <ryan.daoud07@gmail.com>
2020-07-15 16:43:04 +0200

    Première version

---
+++ Calculatrice/src/App.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+	/*
+		 Calculatrice calc = new Calculatrice();
+
+		    
+		    
+		    System.out.println("Rentrez deux valeurs : ");
+		    Scanner sc = new Scanner(System.in);
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+
+		    System.out.println("1) Addition\n2)Soustraction\n3) Multiplication \n4) Division");
+		    System.out.println("Choisissez le calcul que vous voulez effectuer par son numéro : ");
+		    Scanner sca = new Scanner(System.in); 
+		    int op = sca.nextInt();
+
+		    if (op == 1 ){
+		        System.out.println(calc.add(a, b));
+		       
+		    }
+		    else if (op == 2 ){
+		    	System.out.println(calc.sub(a, b));
+		    }
+		    else if (op == 3 ){
+		    	System.out.println(calc.mul(a, b));
+		    }
+		    else if (op == 4 ){
+		    	System.out.println(calc.div(a, b));
+		    }
+		    else {
+		        System.out.println("Cette operation n'est pas disponible");
+		    }
+		    
+		*/
+		    
+		
+		Test test = new Test();
+		test.testAdd();
+		test.testSub();
+		test.testMul();
+		test.testDiv();
+		
+
+	}
+
+}
---
+++ Calculatrice/src/Calculatrice.java
@@ -0,0 +1,29 @@
+public class Calculatrice{
+
+
+    public int add(int a, int b){
+        int resultat;
+        resultat = a + b;
+        return resultat;
+    }
+
+    public int sub(int a, int b){
+        int resultat;
+        resultat = a - b;
+        return resultat;
+    }
+
+    public int mul(int a, int b){
+        int resultat;
+        resultat = a * b;
+        return resultat;
+    }
+
+    public int div(int a, int b){
+        int resultat;
+        resultat = a / b;
+        return resultat;
+    }
+    
+
+}
